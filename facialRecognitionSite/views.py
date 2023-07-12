9f810c8e25499df2f154127080914d6eba2df227
ErnestCrypto <akotobamfo.eab@gmail.com>
2023-07-12 22:39:04 +0000

    commit

--- facialRecognitionSite/views.py
+++ facialRecognitionSite/views.py
@@ -42,7 +42,7 @@ def face(request):
    
     for user in get_user(users):
     # Load a sample picture and learn how to recognize it.
-        sample_image = face_recognition.load_image_file(f"{user.image.url}")
+        sample_image = face_recognition.load_image_file(f"..{user.image.url}")
         sample_image_containers.append(sample_image)
         sample_face_encoding = face_recognition.face_encodings(sample_image)[0]
         

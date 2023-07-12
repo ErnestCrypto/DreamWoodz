9dc974e27fdad9702c7a6500822da7f779c198b1
ErnestCrypto <akotobamfo.eab@gmail.com>
2023-07-12 18:48:24 +0000

    commit

--- DreamWoodz/settings.py
+++ DreamWoodz/settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
